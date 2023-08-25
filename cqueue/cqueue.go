c9fca364766ca4ee258645b261b7397809c2469c
gubailin <1184618238@qq.com>
2023-08-25 15:42:13 +0800

    feat:实现无锁队列

---
+++ cqueue/cqueue.go
@@ -0,0 +1,45 @@
+package cqueue
+
+import (
+	"sync/atomic"
+)
+
+type Queue[T any] struct {
+	Head atomic.Pointer[Node[T]]
+	Tail atomic.Pointer[Node[T]]
+}
+
+type Node[T any] struct {
+	val  T
+	next atomic.Pointer[Node[T]]
+}
+
+func newNode[T any](val T) *Node[T] {
+	return &Node[T]{val: val}
+}
+
+func (q *Queue[T]) Enqueue(val T) {
+	node := newNode[T](val)
+	ptr := atomic.Pointer[Node[T]]{}
+	ptr.Store(node)
+	for {
+		tailVal := q.Tail.Load()
+		if q.Tail.CompareAndSwap(tailVal, node) {
+			return
+		}
+	}
+}
+
+func (q *Queue[T]) Dequeue() T {
+	for {
+		head := q.Head.Load()
+		if head == nil {
+			var res T
+			return res
+		}
+		nextVal := head.next.Load()
+		if q.Head.CompareAndSwap(head, nextVal) {
+			return head.val
+		}
+	}
+}
